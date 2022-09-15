2a5b050ab9c64e4346bb2991a38bcb8d3ad71bf8
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2022-09-15 23:37:40 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const animals = document.querySelectorAll('h2');
+console.log(`Number of categories:`, animals.length);
+
+
+const animalsContent = document.querySelector('h2');
+console.log(`Category:`, animalsContent.textContent);
+const animalsEl = document.querySelector('.item');
+console.log(`Elements:`, animalsEl.length);
