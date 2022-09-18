550c8413a6f6bc938463886a9e5837719f83ec60
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2022-09-18 23:17:37 +0300

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+const textColor = document.querySelector(".color");
+const changeBtnColor = document.querySelector(".change-color");
+
+changeBtnColor.addEventListener("click", changeBackgroundColor);
+
+function changeBackgroundColor() {
+  const ourColor = getRandomHexColor();
+  body.style.backgroundColor = `${ourColor}`;
+  textColor.textContent = `${ourColor}`;
+}
