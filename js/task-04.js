c88fe0c140f3442b04ab8aeb74dbd348a98f0bb8
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2022-09-18 15:49:39 +0300

    task4

--- js/task-04.js
+++ js/task-04.js
@@ -1,20 +1,17 @@
-let counter = 0;
+let counterValue = 0;
 
-const buttonRef = document.querySelector("#counter");
-const decrementBtn = buttonRef.firstElementChild;
-const incrementBtn = buttonRef.lastElementChild;
-const counterValue = document.querySelector("#value");
+const elementBtn = document.querySelector("#counter");
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const counter = document.querySelector("#value");
 
 decrementBtn.addEventListener("click", countDecrement);
 incrementBtn.addEventListener("click", countIncrement);
-
-
-function countIncrement() {
-  counter += 1;
-  counterValue.textContent = counter;
-}
-
 function countDecrement() {
-  counter -= 1;
-  counterValue.textContent = counter;
+  counterValue -= 1;
+  counter.textContent = counterValue;
+}
+function countIncrement() {
+  counterValue += 1;
+  counter.textContent = counterValue;
 }
