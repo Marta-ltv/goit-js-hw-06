77b58976cb6b5d7b3e78128237552de81c480ce8
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2022-09-18 23:04:45 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,33 @@
+const checkForm = document.querySelector(".login-form");
+console.log(checkForm);
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert(`All fields must be filled!`);
+    return;
+  }
+
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+
+    console.log(userData);
+    event.currentTarget.reset();
+};
+
+checkForm.addEventListener("submit", handleSubmit);
+
+
+    
+
+
+
+
+   
